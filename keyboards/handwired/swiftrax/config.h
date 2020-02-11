ea960686bd69b5dd61b67108b8644b4a33f88cae
Swiftrax <swiftrax@gmail.com>
2020-02-10 19:48:32 -0800

    Attempt to fix bugs

--- keyboards/handwired/swiftrax/config.h
+++ keyboards/handwired/swiftrax/config.h
@@ -14,8 +14,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-//#ifndef CONFIG_H
-//#define CONFIG_H
 
 #include "config_common.h"
 
@@ -51,8 +49,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
