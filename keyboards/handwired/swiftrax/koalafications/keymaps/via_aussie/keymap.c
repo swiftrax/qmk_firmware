b72551dcebe4072467a48cd6560ca89c9e504c42
Swiftrax <swiftrax@gmail.com>
2021-04-29 22:21:49 -0700

    working!

--- keyboards/handwired/swiftrax/koalafications/keymaps/via_aussie/keymap.c
+++ keyboards/handwired/swiftrax/koalafications/keymaps/via_aussie/keymap.c
@@ -26,6 +26,12 @@ enum custom_keycodes {
     KC_NOMODE,
 };
 
+void eeconfig_init_user(void) {
+#ifdef UNICODE_ENABLE
+    set_unicode_input_mode(UC_WINC);
+#endif
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    [0] = LAYOUT_all(
     KC_ESC , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
@@ -328,8 +334,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 typing_mode = keycode;
             }
             return false;
-        if (typing_mode == KC_AUSSIE) {
-        return process_record_aussie(keycode, record);
-        }    
     }
+    if (typing_mode == KC_AUSSIE) {
+        return process_record_aussie(keycode, record);
+    }    
+    return true;
 }
--- keyboards/handwired/swiftrax/koalafications/keymaps/via_aussie/rules.mk
+++ keyboards/handwired/swiftrax/koalafications/keymaps/via_aussie/rules.mk
@@ -1,1 +1,2 @@
 VIA_ENABLE = yes
+UNICODE_ENABLE = yes
